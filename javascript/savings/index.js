8886753d14c8e1a3b7843245c1631146329403e0
Emily Raspe <emilyraspe@icloud.com>
2024-02-09 16:07:26 +0100

    edit js

---
+++ javascript/savings/index.js
@@ -0,0 +1,11 @@
+const startingBalance = 1000;
+let monthlyContribution = 50;
+const months = 12;
+
+let totalSavings = startingBalance + monthlyContribution * months;
+
+console.log(
+  "After 12 months of savings I have ",
+  totalSavings,
+  "amount of money"
+);
