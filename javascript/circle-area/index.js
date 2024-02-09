820e9eda82d23a42e615ebcf2be2fb022d88009a
Emily Raspe <emilyraspe@icloud.com>
2024-02-09 16:40:06 +0100

    edit js

---
+++ javascript/circle-area/index.js
@@ -0,0 +1,5 @@
+let circleRadius = 5;
+let circumference = 2 * Math.PI * circleRadius;
+let area = Math.PI * Math.pow(circleRadius, 2);
+
+console.log("The area is", area, "and the circumference", circumference);
