be8c3c91c7ea54af7725fea96c0780bb067ca87c
Emily Raspe <emilyraspe@icloud.com>
2024-02-09 15:53:33 +0100

    edit js

---
+++ javascript/tip-calculator/index.js
@@ -0,0 +1,8 @@
+let mealCost = 25;
+let tipPercentage = 10;
+
+let tipAmount = (mealCost / 100) * tipPercentage;
+
+totalCost = mealCost + tipAmount;
+
+console.log("The total cost fo your meal is ", totalCost);
