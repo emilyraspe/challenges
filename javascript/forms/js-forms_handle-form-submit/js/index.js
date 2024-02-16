2eef1ea507947267f92f020fb25f4fb1c7e3d0c3
Emily Raspe <emilyraspe@icloud.com>
2024-02-16 11:31:20 +0100

    download challenges

---
+++ javascript/forms/js-forms_handle-form-submit/js/index.js
@@ -0,0 +1,19 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const formData = new FormData(evt.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  // age-badness-sum
+  let ageBadnessSum =
+    parseInt(evt.target.elements.age.value) +
+    parseInt(evt.target.elements.badness.value);
+  console.log("The age-badness-sum is", ageBadnessSum);
+
+  evt.target.elements.firstName.focus();
+  evt.target.reset();
+});
