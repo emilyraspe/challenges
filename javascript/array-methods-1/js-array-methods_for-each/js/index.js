d96d937115ebf9733632239a30f8a300e3f72db9
Emily Raspe <emilyraspe@icloud.com>
2024-02-21 15:04:15 +0100

    task2 for for-each challenge

--- javascript/array-methods-1/js-array-methods_for-each/js/index.js
+++ javascript/array-methods-1/js-array-methods_for-each/js/index.js
@@ -19,9 +19,21 @@ const colors = [
   "#A5A5A5",
 ];
 
+//TASK 1
+/*
 colors.forEach((color) => {
   const div = document.createElement("div");
   div.classList.add("color-box");
   div.style.backgroundColor = color;
   document.body.append(div);
-});
+});*/
+
+//TASK 2
+colors.forEach((color) => renderColorBox(color));
+
+function renderColorBox(color) {
+  const div = document.createElement("div");
+  div.classList.add("color-box");
+  div.style.backgroundColor = color;
+  document.body.append(div);
+}
