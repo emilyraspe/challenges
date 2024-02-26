a70560d8e41761d5724ef8c2f9311c6baa1b3579
Emily Raspe <emilyraspe@icloud.com>
2024-02-26 11:47:01 +0100

    make changes to darkmode

--- javascript/asyncFunctions/js-async-functions_sleep/js/index.js
+++ javascript/asyncFunctions/js-async-functions_sleep/js/index.js
@@ -7,27 +7,21 @@ const body = document.body;
 // --v-- code here --v--
 
 button3.addEventListener("click", async () => {
-  body.classList.add("dark");
   icon.textContent = "💤";
   await sleep(3);
   icon.textContent = "⏰";
-  body.classList.remove("dark");
 });
 
 button5.addEventListener("click", async () => {
-  body.classList.add("dark");
   icon.textContent = "💤";
   await sleep(5);
   icon.textContent = "⏰";
-  body.classList.remove("dark");
 });
 
 async function handleSleepFor10Button() {
-  body.classList.add("dark");
   icon.textContent = "💤";
   await sleep(10);
   icon.textContent = "⏰";
-  body.classList.remove("dark");
 }
 
 button10.addEventListener("click", handleSleepFor10Button);
@@ -36,8 +30,12 @@ button10.addEventListener("click", handleSleepFor10Button);
 
 // returns a Promise that resolves after x seconds
 function sleep(seconds) {
+  body.classList.add("dark");
   const sleepPromise = new Promise((resolve) => {
-    setTimeout(() => resolve(), seconds * 1000);
+    setTimeout(() => {
+      body.classList.remove("dark");
+      resolve();
+    }, seconds * 1000);
   });
   return sleepPromise;
 }
