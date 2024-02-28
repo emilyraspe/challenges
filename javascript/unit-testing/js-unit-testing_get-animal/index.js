1ab012dabad3fd3130c8a788361ec78d8119fe43
Emily Raspe <emilyraspe@icloud.com>
2024-02-28 10:51:28 +0100

    edit js for get animal

--- javascript/unit-testing/js-unit-testing_get-animal/index.js
+++ javascript/unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return `I totally love ${animal}!`;
+  } else if (animal === undefined) {
+    return "I do not like animals at all!";
+  } else if (typeof animal === "string") {
+    return `I like ${animal}!`;
+  }
+}
