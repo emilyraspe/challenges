1423ab45bd0b622ceeee5ef1ca83691b0c5232b8
Emily Raspe <emilyraspe@icloud.com>
2024-03-04 15:51:41 +0100

    article component

--- react/basics/react-basics_attributes/src/App.jsx
+++ react/basics/react-basics_attributes/src/App.jsx
@@ -8,6 +8,10 @@ function Article() {
   return (
     <article className="article">
       <h2 className="article__title">Heading</h2>
+      <label htmlFor="question">Is this a question</label>
+      <input id="question"></input>
+
+      <a href="https://react.dev/">Click here</a>
     </article>
   );
 }
