0bf3b7640c997ebbff34281514ee71376c4f4e69
Emily Raspe <emilyraspe@icloud.com>
2024-03-05 10:48:59 +0100

    edit js for react sum

--- react/props/react-props_sum/src/App.jsx
+++ react/props/react-props_sum/src/App.jsx
@@ -1,5 +1,14 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={2} valueB={3} />;
+}
+
+function Sum({ valueA, valueB }) {
+  let sum = valueA + valueB;
+  return (
+    <h1>
+      {valueA} + {valueB} = {sum}
+    </h1>
+  );
 }
