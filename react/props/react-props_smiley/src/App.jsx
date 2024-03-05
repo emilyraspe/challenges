84c6f48646999fd180c272a7ce80a83a51cc94a6
Emily Raspe <emilyraspe@icloud.com>
2024-03-05 10:57:35 +0100

    edit js for react smiley challenge

--- react/props/react-props_smiley/src/App.jsx
+++ react/props/react-props_smiley/src/App.jsx
@@ -1,5 +1,9 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy={true} />;
+}
+
+function Smiley({ isHappy }) {
+  return <h1>{isHappy ? "😊" : "😭"}</h1>;
 }
