20f6ccb02996295f905fd69772f2781b0a6917d7
Emily Raspe <emilyraspe@icloud.com>
2024-03-06 15:07:25 +0100

    edit text state

--- react/state/react-state_box/src/App.jsx
+++ react/state/react-state_box/src/App.jsx
@@ -3,16 +3,18 @@ import { useState } from "react";
 
 export default function App() {
   const [isActive, setIsActive] = useState(false);
+  const [text, setText] = useState("Activate");
 
   function handleClick() {
     setIsActive(!isActive);
+    setText(isActive ? "Activate" : "Deactivate");
     console.log(isActive);
   }
 
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button onClick={handleClick}>{text}</button>
     </main>
   );
 }
