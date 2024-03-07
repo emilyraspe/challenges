2247f73fdb6f3da1ccf8565a2c4adae066f36c88
Emily Raspe <emilyraspe@icloud.com>
2024-03-07 11:28:45 +0100

    fruits challenge

--- react/arrays/react-with-arrays_fruits/src/App.jsx
+++ react/arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,13 +1,44 @@
 import Card from "./components/Card";
 import "./App.css";
 
-
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1,
+      name: "🍌 Banana",
+      color: "orange",
+    },
+
+    {
+      id: 2,
+      name: "🍓 Strawberry",
+      color: "red",
+    },
+
+    {
+      id: 3,
+      name: "🍎 Apple",
+      color: "red",
+    },
+
+    {
+      id: 4,
+      name: "🍐 Pear",
+      color: "green",
+    },
+
+    {
+      id: 5,
+      name: "🍊 Orange",
+      color: "orange",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map(({ id, name, color }) => (
+        <Card key={id} name={name} color={color} />
+      ))}
     </div>
   );
 }
