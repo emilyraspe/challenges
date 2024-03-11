28bb99c6b4b8de90ccbbe7ac56fecc2ce88a431c
Emily Raspe <emilyraspe@icloud.com>
2024-03-11 11:01:36 +0100

    table-reservation

--- react/state-2/react-state-2_table-reservation/src/components/Counter.jsx
+++ react/state-2/react-state-2_table-reservation/src/components/Counter.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
 import "./Counter.css";
 
-
-export default function Counter() {
-  const [people, setPeople] = useState(0);
+export default function Counter({ people, setPeople }) {
+  function hanldeAdding() {
+    setPeople(people + 1);
+  }
+  function handleSubtracting() {
+    setPeople(people - 1);
+  }
   return (
     <>
       <h2>How many people would you like to visit us with?</h2>
@@ -12,7 +15,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="increment people count"
-          onClick={() => setPeople(people + 1)}
+          onClick={hanldeAdding}
         >
           +
         </button>
@@ -20,7 +23,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement people count"
-          onClick={() => setPeople(people - 1)}
+          onClick={handleSubtracting}
         >
           -
         </button>
