237f5c2d4bb0809a87debda9ab4ea2e0b46e6d1a
Emily Raspe <emilyraspe@icloud.com>
2024-03-18 16:35:39 +0100

    add Dynamic Routes

--- next.js/lotr-app/pages/volumes.js
+++ next.js/lotr-app/pages/volumes.js
@@ -1,29 +1,35 @@
-import { introduction } from "../lib/data";
+import { introduction, volumes } from "../lib/data";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Volumes() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
   return (
     <div>
       <h1>Lord of the Rings</h1>
       <p>{introduction}</p>
       <h2>All Volumes</h2>
       <ul>
-        <li>
-          <Link href="/volumes/the-fellowship-of-the-ring">
-            The fellowship of the ring
-          </Link>
-        </li>
-
-        <li>
-          <Link href="/volumes/the-two-towers">The two towers</Link>
-        </li>
-
-        <li>
-          <Link href="/volumes/the-return-of-the-king">
-            The return of the king
-          </Link>
-        </li>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
       </ul>
+      <button
+        onClick={() => {
+          const random = getRandomElement(volumes);
+          router.push(`/volumes/${random.slug}`);
+        }}
+      >
+        Random Volume
+      </button>
     </div>
   );
 }
---
+++ next.js/lotr-app/pages/volumes/[slug].js
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import { volumes } from "../../lib/data";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function VolumeDetail() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const volume = volumes.find((volume) => slug === volume.slug);
+  if (!volume) return;
+  const { books } = volume;
+
+  return (
+    <div>
+      <Link href="/volumes">← All Volumes</Link>
+      <h1>{volume.title}</h1>
+      <p>{volume.description}</p>
+      <Image
+        src={volume.cover}
+        height={230}
+        width={140}
+        alt="the-two-towers Cover"
+      ></Image>
+      <ul>
+        {books.map((book) => (
+          <li key={book.ordinal}>
+            <p>
+              {book.ordinal}: {book.title}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
