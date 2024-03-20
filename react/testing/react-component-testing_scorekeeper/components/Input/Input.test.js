5a36609ea407ea35205fa6f78ec8f34d674775b7
Emily Raspe <emilyraspe@icloud.com>
2024-03-20 14:19:35 +0100

    start challenge: input and nav done

--- react/testing/react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react/testing/react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,19 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  render(<Input></Input>);
+  /* const input = screen.getByLabelText("value");
+  expect(input).toHaveAttribute("name", "value"); */
+});
 
-test("calls callback on every user input", async () => {});
+test("calls callback on every user input", async () => {
+  const onChange = jest.fn();
+  const user = userEvent.setup();
+  render(<Input onChange={onChange}></Input>);
+
+  const input = screen.getByRole("textbox");
+  await user.type(input, "my game");
+
+  expect(onChange).toHaveBeenCalledTimes(7);
+});
